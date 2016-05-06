5a5fca0d1975f32c03a6684ffae38259c9ef3519
Hall, Jeff <Jeffrey.Hall@exlibrisgroup.com>
2016-05-06 09:52:44 -0700

    Adding new classes

--- src/main/java/timbuchalka/Main.java
+++ src/main/java/timbuchalka/Main.java
@@ -3,35 +3,16 @@ package timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-//        calcFeetAndInchesToCentimeters(6, 0);
-//        calcFeetAndInchesToCentimeters(7, 5);
-//        calcFeetAndInchesToCentimeters(-10, 1);
-//        calcFeetAndInchesToCentimeters(0, 1);
-//        calcFeetAndInchesToCentimeters(6, 13);
-        calcFeetAndInchesToCentimeters(157);
-        calcFeetAndInchesToCentimeters(-10);
+
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.setModel("Carrera");
+        System.out.println("Model is " + porsche.getModel());
+
+        holden.setModel("Monkey House");
+        System.out.println("Model is " + holden.getModel());
     }
 
-    private static double calcFeetAndInchesToCentimeters(double feet, double inches) {
-        if (feet >= 0 && (inches >= 0 && inches <= 12)) {
-            double totalInches = ((12d * feet) + inches);
-            double centimeters = (2.54d * totalInches);
-            System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
-            return centimeters;
-        } else {
-            System.out.println("Invalid feet or inches parameters");
-            return -1;
-        }
-    }
-
-    private static double calcFeetAndInchesToCentimeters(double inches) {
-        if (inches < 0) {
-            return -1;
-        }
-
-        double feet = (int) (inches / 12);
-        double remainingInches = (int) (inches % 12d);
-        return calcFeetAndInchesToCentimeters(feet, remainingInches);
-    }
 
 }
